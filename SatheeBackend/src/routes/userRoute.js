b3313c96d4674ec6537723866aac06ba19a5e2a4
Avash Dahal <aavashdahal2@gmail.com>
2025-04-10 17:39:47 +0545

    Added LLMcontroller for llm api

--- SatheeBackend/src/routes/userRoute.js
+++ SatheeBackend/src/routes/userRoute.js
@@ -1,14 +1,17 @@
 import express from 'express';
 import { getAllUsers, getUser, updateUser } from '../controllers/userController.js';
-
+import  chatWithLLM  from '../controllers/llmController.js';
+import { protect } from '../middlewares/authMiddleware.js';
 const router = express.Router();
 
-
+router.post('/chat', protect,chatWithLLM);
  
 // User routes
 router.get('/', getAllUsers);
 router.get('/:id', getUser);
 router.patch('/:id', updateUser);
+
+
 
 //profile Completion
 
